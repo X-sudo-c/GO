80f85c07d4e9eeea7f661d39330d2700e926a9da
X-sudo-c <x1dragonn@gmail.com>
2025-05-01 21:51:38 +0000

    go

---
+++ fmtpackage.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+
+
+
+
+
+
+func main(){
+
+	fmt.Println("HI")
+	fmt.Println("Lets first see how", "Println() method works.")
+	fmt.Println("Notice that each statement adds a new line for us.")
+	fmt.Println("There is also no default space", " between the strings arguments.")
+
+
+	fmt.Print("DIfferent", "STorys, wide, lol") //the fmt.Print state ments act alittle weird 
+
+}
+
