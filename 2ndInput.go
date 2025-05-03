4e62f3537c3cde1dfc5beb2a879308e17e0cc2fa
X-sudo-c <x1dragonn@gmail.com>
2025-05-03 22:41:23 +0000

    223ksdj

---
+++ 2ndInput.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+
+func main(){
+	var like string
+
+	fmt.Println("Type ins a ny random")
+	fmt.Scan(&like)
+	
+	fmt.Printf("Well i guess the printing worked bcus this is the value %v", like)
+
+
+
+}
