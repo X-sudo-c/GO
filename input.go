b6ef884df2eb4f97f68942371bced1391df8a9ae
X-sudo-c <x1dragonn@gmail.com>
2025-05-03 22:26:23 +0000

    damn

---
+++ input.go
@@ -0,0 +1,23 @@
+package main
+
+
+import "fmt"
+
+func main(){
+
+	
+	fmt.Println("How are you doing ?") //First we print how are you
+
+	var response1 string  //define a variable call it response
+	var response2 string 
+
+	fmt.Scan(&response1) //Ask for input i guess
+	fmt.Scan(&response2)
+
+	fmt.Printf("Im %v %v.", response1, response2)//print the output
+
+
+
+
+
+}
