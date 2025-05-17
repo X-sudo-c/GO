1de6c526d26413b05a0f05fcc898bfa668a42d59
X-sudo-c <x1dragonn@gmail.com>
2025-05-17 22:33:59 +0000

    functions

---
+++ functions.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+
+func summonNicole(){
+	fmt.Println("Hey nicole, get over here! \n")
+}
+
+
+
+
+
+func main(){
+	//call your function her
+
+	summonNicole()
+	fmt.Println("Crazy \n")
+
+	//we call our function for the second time
+	summonNicole()
+
+
+}
