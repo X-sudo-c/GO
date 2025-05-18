171cab1564a404e555b9804d61b280f5f8951208
X-sudo-c <x1dragonn@gmail.com>
2025-05-18 19:28:30 +0000

    scopes in variables

---
+++ scope.go
@@ -0,0 +1,16 @@
+package main
+
+
+import "fmt"
+//basically defining scopes in functions
+
+func startGame(){
+
+	instructions := "Press enter to start..."
+	fmt.Println(instructions)
+}
+
+
+func main(){
+	startGame()
+}
