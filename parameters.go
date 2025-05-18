60ae01ed7a47b2513d2472af94a2d92e683466a3
X-sudo-c <x1dragonn@gmail.com>
2025-05-18 19:26:11 +0000

    using parameters

---
+++ parameters.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func multiplier(x, y int32) int32{
+	return x * y  //this returns the multiples  of the parameters in the functions
+}
+
+
+func main(){
+	fmt.Println(multiplier(3,3))
+
+	//This is a simple and easy implementation
+}
