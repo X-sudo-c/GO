83c616029fcb07427426e1c2375ba41d2e14060a
X-sudo-c <x1dragonn@gmail.com>
2025-06-08 00:03:02 +0000

    arghh what am i doin

--- 2ndInput.go
+++ 2ndInput.go
@@ -11,6 +11,11 @@ func main(){
 	
 	fmt.Printf("Well i guess the printing worked bcus this is the value %v", like)
 
-
+	//bomboclat krokro clat
 
 }
+
+
+
+//arghhghghghhg i have to actualy learn go well
+//dammit mernn
